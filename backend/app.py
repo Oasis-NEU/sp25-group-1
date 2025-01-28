be3e1146f4553d2d655dc24734bfcf7a4b6d91ab
Aryan0102 <aryansumit0102@gmail.com>
2025-01-27 22:27:04 -0500

    Added more information in app.py

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,11 @@ load_dotenv()
 # incomplete app.py will finish it later
 # - Aryan
 
+# Also guys, please make sure to install the required packages
+# You should just be able to git pull everything?
+# Also DO NOT WORK on the main branch, create a new branch and work on that
+# - Aryan
+
 # To do
 # 1. Create a database connection
 # 2. Flask Blueprints for different routes
