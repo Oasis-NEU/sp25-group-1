56738e069a496719fa6d38b2052e46845ad46419
Aryan0102 <aryansumit0102@gmail.com>
2025-02-02 18:17:15 -0500

    Worked on signin page

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const SignIn = () => {
   return (
     <div className="h-screen backgroundBlue flex items-center justify-center">
         <div className="w-[50%] h-[50%] navbarColor rounded-lg flex flex-col items-center">
-            <div className="postTitleColor rounded-lg flex px-[2%] w-[50%] h-[13%] mt-[7%] justify-center">
-                <p className="text-white text-[175%]">Enter Sign In Information</p>
+            <div className="postTitleColor rounded-lg flex w-[50%] h-[10%] mt-[10%] justify-center">
+                <p className="text-white text-[175%]">Sign In</p>
             </div>
             <div className="bg-white rounded-lg flex px-[2%] w-[80%] h-[8%] mt-[5%] justify-center">
                 <p className="text-black text-[125%]">Enter your email address here:</p>
